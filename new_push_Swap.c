2121a5771f66be7d4276ebb75659df832d918264
sandRiches <richrules2@hotmail.com>
2019-07-25 19:49:59 +0200

    kippen

--- new_push_Swap.c
+++ new_push_Swap.c
@@ -12,8 +12,6 @@
 
 #include "push_swap.h"
 
-
-
 void	check_swap(t_stacks *info)
 {
 	if (LEN_A >= 2)
@@ -28,27 +26,36 @@ void	check_swap(t_stacks *info)
 	}
 }	
 
-void		recursion(t_stacks *info, int begin_len, int last_push)
+void	recursion(t_stacks *info, int begin_len, int amt_last_to_a, int last_push)
 {
 	int nb;
 	int pivot;
 	int closest;
 	char op_to_closest[4];
+	int amt_last_to_b;
 
+	amt_last_to_b = 0;
 	nb = begin_len;
-	while (nb / 2 > last_push)
+	if (LEN_B == 0 || (nb / 2 == last_push))
+		return ;
+	while (nb / 2 > last_push && nb / 2 >= amt_last_to_a)
 	{
 		nb /= 2;
 	}
 	pivot = find_nbr_n(B, nb, ft_max(nb / 2, 1));
 	closest = find_closest_B(info, pivot, op_to_closest);
 	ft_printf("closest: %i\npivot: %i\n", closest, pivot);
-	while (closest >= pivot)
+	while (closest >= pivot && LEN_B > 0)
     {
 		push_closest_a(info, closest, op_to_closest);
     	closest = find_closest_B(info, pivot, op_to_closest);
+		amt_last_to_b++;
+		ft_printf("closest: %i\npivot: %i\n", closest, pivot);
+		check_swap(info);
+		print_stacks(info);
     }
-	
+	//check_swap(info);
+	recursion(info, begin_len, amt_last_to_a, amt_last_to_b);
 }
 
 void	initial_push(t_stacks *info)
@@ -57,10 +64,12 @@ void	initial_push(t_stacks *info)
     int closest;
     char op_to_closest[4];
 	int begin_len;
+	int amt_last_to_a;
 
 	begin_len = LEN_A;
     while (LEN_A > 2)
     {
+		amt_last_to_a = 0;
 		print_stacks(info);
         pivot = find_nbr_n(A, LEN_A, (LEN_A / 2) + 1);
         closest = find_closest_A(info, pivot, op_to_closest);
@@ -68,20 +77,9 @@ void	initial_push(t_stacks *info)
         {
             push_closest_b(info, closest, op_to_closest);
             closest = find_closest_A(info, pivot, op_to_closest);
+			amt_last_to_a++;
         }
+		check_swap(info);
     }
-	check_swap(info);
-	recursion(info, begin_len, 0);
+	recursion(info, begin_len, amt_last_to_a, 0);
 }
-
-// int      recursion(int nummer, t_stacks *info)
-// {
-
-// }
-
-// void    new_push_swap(t_stacks *info)
-// {
-//     int pivot;
-
-//     pivot = find_nbr_n(A, LEN_A, LEN_A / 2);
-// }
